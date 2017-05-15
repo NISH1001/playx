31089468d7441f43835ac075725ae163a01b185c
NISH1001 <nishanpantha@gmail.com>
2017-05-15 09:32:43 +0545

    Change ffmpeg conversion parameters to speed up the conversion

--- utility.py
+++ utility.py
@@ -45,6 +45,7 @@ def convert_to_mp3(filename):
     print("Converting to mp3...Have patience...")
     new_file = str(time.time()) + filename
     cli = "ffmpeg -i {0} {1}".format(filename, new_file)
+    #cli = "ffmpeg -i {0} -map 0:a:0 -b:a 96k {1}".format(filename, new_file)
     output, error = exe(cli)
     shutil.move(new_file, filename)
 
