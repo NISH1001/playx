0bbe26574d9d0ee84ef5ec0d2a08d988262a2d82
deepjyoti30 <deep.barman30@gmail.com>
2018-07-14 22:03:46 +0530

    Forgot to add playlist.py

---
+++ playlist.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Definitions to interact with playlists."""
+
+from main import stream
+from utility import isON
+from time import sleep
+
+
+def check_type(line):
+    """Check if the line has a url or song name."""
+    if 'http' in line:
+        return 'url'
+    else:
+        return 'name'
+
+
+def read_playlist(file):
+    """Read the playlist and play songs accordingly."""
+    song_stream = open(file, 'r')
+    while True:
+        # Remove \n if present
+        song = song_stream.readline()
+        if not song:
+            break
+        song = song.replace('\n', '')
+        type = check_type(song)
+        stream(type, song)
+        # show_menu()
+        while isON():
+            sleep(0.5)
