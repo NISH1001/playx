7a7f19aeac3a908f8327955a22a66ae32723bac6
Tucker Boniface <tucker@my.innovationspokane.org>
2018-07-28 15:05:53 -0700

    exit with error message if no results found

--- songfinder.py
+++ songfinder.py
@@ -3,9 +3,11 @@
 
 from youtube import search_youtube
 
-
 def search(song):
     """Search the song in youtube."""
     videos = search_youtube(song)
-    video = videos[0]
+    try:
+        video = videos[0]
+    except IndexError:
+        return None
     return video
