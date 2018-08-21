6e2f66418e5e9fe8f2e27c2c8cb0fe70ef44d486
Matt Marcus <mattmarcus58@gmail.com>
2018-08-20 20:37:28 -0600

    update mvce setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ with open('requirements.txt') as r:
 
 setup(name='playx',
       packages=['playx'],
+      author='NISH1001',
       entry_points={
           'console_scripts': [
               'playx = playx.main:main'
           ]
       },
-      version='0.0.1', # need input
+      version='0.0.1',
       license=license,
-
+      install_requires=requirements,
       )
