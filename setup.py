953eb39f5a525ac1cba634197b44e219636d865e
NISH1001 <nishanpantha@gmail.com>
2018-08-22 07:25:26 +0545

    Add more details to setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,19 @@ with open('LICENSE') as l:
 with open('requirements.txt') as r:
     requirements = r.read().split('\n')
 
-setup(name='playx',
-      packages=['playx'],
-      author='NISH1001',
-      entry_points={
-          'console_scripts': [
-              'playx = playx.main:main'
-          ]
-      },
-      version='0.0.1',
-      license=license,
-      install_requires=requirements,
-      )
+setup(
+    name='playx',
+    packages=['playx'],
+    author='NISH1001',
+    author_email='nishanpantha@gmail.com',
+    description='Search and play any song from terminal.',
+    url='http://github.com/NISH1001/playx',
+    entry_points={
+        'console_scripts': [
+            'playx = playx.main:main'
+        ]
+    },
+    version='0.0.1',
+    license=license,
+    install_requires=requirements,
+)
