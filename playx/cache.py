839fa414a965f0841630e422f4c87e65e57cc0a0
NISH1001 <nishanpantha@gmail.com>
2019-06-09 14:07:50 +0545

    Move urls json path to log directory

--- playx/cache.py
+++ playx/cache.py
@@ -146,24 +146,32 @@ def search_URL(URL):
     """
     Check if the URL is cached.
     """
-    file_path = os.path.expanduser('~/.playx/songs/mapURL.json')
+    file_path = os.path.expanduser('~/.playx/logs/urls.json')
     logger.debug(URL)
-    RSTREAM = open(file_path, 'r')
-    data = json.load(RSTREAM)
-    logger.info("Searching {} in the cached file".format(URL))
+
+    # check if file exists
+    if not os.path.exists(file_path):
+        temp = open(file_path, 'w')
+        temp.close()
+        data = {}
+
     try:
-        return data[URL]
-    except KeyError:
-        return None
+        with open(file_path, 'r') as RSTREAM:
+            data = json.load(RSTREAM)
+            logger.info("Searching {} in the cached file".format(URL))
+    except JSONDecodeError:
+        data = {}
+    return data.get(URL, None)
 
 
 def update_URL_cache(title, URL):
     """
     Update the URL cache saved in the mapURL.json file.
     """
-    cache_dir = os.path.expanduser('~/.playx/songs')
-    file_path = os.path.join(cache_dir, 'mapURL.json')
-    song_path = os.path.join(cache_dir, fix_title(title))
+    log_dir = os.path.expanduser('~/.playx/logs')
+    songs_dir = os.path.expanduser('~/.playx/songs')
+    file_path = os.path.join(log_dir, 'urls.json')
+    song_path = os.path.join(songs_dir, fix_title(title))
 
     if not os.path.exists(file_path):
         temp = open(file_path, 'w')
@@ -173,13 +181,12 @@ def update_URL_cache(title, URL):
         with open(file_path, 'r') as RSTREAM:
             try:
                 data = json.load(RSTREAM)
-            except JSONDecodeError:
-                data = {}
+            except JSONDecodeError: data = {}
 
     data.update({URL: song_path})
 
-    WSTREAM = open(file_path, 'w')
-    json.dump(data, WSTREAM)
+    with open(file_path, 'w') as WSTREAM:
+        json.dump(data, WSTREAM)
 
 
 if __name__ == "__main__":
