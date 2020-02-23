d44bd5a94e6b066931a4bac77df6cfc8cb33d131
NISH1001 <nishanpantha@gmail.com>
2020-02-23 12:07:44 +0545

    Use youtube-dl to fetch playlist information
    Add `YoutubePlaylist2` that uses `youtube-dl` to fetch all the
    information.

--- playx/playlist/youtube.py
+++ playx/playlist/youtube.py
@@ -2,25 +2,25 @@
 defined.
 """
 
+import json
 import requests
 from bs4 import BeautifulSoup
 import re
 
-from playx.playlist.playlistbase import (
-    PlaylistBase, SongMetadataBase
-)
+from playx.utility import exe
+
+from playx.playlist.playlistbase import PlaylistBase, SongMetadataBase
 
 from playx.logger import Logger
 
 
 # Setup logger
-logger = Logger('YoutubePlaylist')
+logger = Logger("YoutubePlaylist")
 
 
 class YoutubeMetadata(SongMetadataBase):
-
-    def __init__(self, url='', title=''):
-        super().__init__(title, url, '')
+    def __init__(self, url="", title=""):
+        super().__init__(title, url, "")
         self._create_search_query()
 
     def _create_search_query(self):
@@ -35,26 +35,32 @@ class YoutubeMetadata(SongMetadataBase):
 
 
 class YoutubePlaylist(PlaylistBase):
-    """Class to store YouTube playlist data."""
+    """
+        Class to store YouTube playlist data.
+        This is where we try to parse ourselves the results of the pages.
+        Only works for first 100 songs.
+        If we want to fetch more, we have to do other ajax request or simulate
+        scrolling which is another problem.
+        Refer to `YoutubePlaylist2`
+    """
 
     def __init__(self, URL, pl_start=None, pl_end=None):
         """Init the URl."""
         super().__init__(pl_start, pl_end)
         self.URL = URL
         self.list_content_tuple = []
-        self.playlist_name = ''
+        self.playlist_name = ""
         self._DELETED = [
-                            'deleted video',
-                            'मेटाइएको भिडियो',
-                            'private video',
-                        ]
+            "deleted video",
+            "मेटाइएको भिडियो",
+            "private video",
+        ]
 
     def extract_name(self, name):
         """Extract the name of the playlist."""
-        name = str(name).replace('\n', '')
-        name = ''.join(re.findall(r'>.*?<', name)).replace('>',
-                                                           '').replace('<', '')
-        name = ' '.join(re.findall(r'[^ ]+', name))
+        name = str(name).replace("\n", "")
+        name = "".join(re.findall(r">.*?<", name)).replace(">", "").replace("<", "")
+        name = " ".join(re.findall(r"[^ ]+", name))
         self.playlist_name = name
 
     def _is_connection_possible(self):
@@ -71,33 +77,35 @@ class YoutubePlaylist(PlaylistBase):
 
     def _check_valid(self, url):
         """Check if the passed URL is valid."""
-        h = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux \
-            x86_64; rv:49.0) Gecko/20100101 Firefox/49.0'}
-        s = BeautifulSoup(requests.get(url, headers=h).text, 'lxml')
+        h = {
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux \
+            x86_64; rv:49.0) Gecko/20100101 Firefox/49.0"
+        }
+        s = BeautifulSoup(requests.get(url, headers=h).text, "lxml")
         t = 'window["ytInitialData"] = '
-        i = next((i for i in s.find_all('script') if t in str(i)))
-        i = i.get_text().replace(t, '').replace('\n', '')
-        i = re.sub(r'^.*"playabilityStatus"', '', i)
-        i = i.split(',')
-        status = re.sub(r':|\{|"|status', '', i[0])
+        i = next((i for i in s.find_all("script") if t in str(i)))
+        i = i.get_text().replace(t, "").replace("\n", "")
+        i = re.sub(r'^.*"playabilityStatus"', "", i)
+        i = i.split(",")
+        status = re.sub(r':|\{|"|status', "", i[0])
         if status == "OK":
             return True
         else:
             reason = next((r for r in i if '"reason"' in r))
-            reason = re.sub(r':|\{|"|reason|simpleText|}', '', reason)
+            reason = re.sub(r':|\{|"|reason|simpleText|}', "", reason)
             logger.info("Skipping {}: {} {}".format(url, status, reason))
             return False
 
     def extract_playlistdata(self):
         """Extract all the videos into YoutubeMetadata objects."""
-        url_prepend = 'https://www.youtube.com/watch?v='
-        url_base = 'https://www.youtube.com'
+        url_prepend = "https://www.youtube.com/watch?v="
+        url_base = "https://www.youtube.com"
         if not self._is_connection_possible():
             logger.warning("Cannot play playlist. No connection detected!")
-            return 'N/A', []
+            return "N/A", []
         r = requests.get(self.URL)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        name = soup.findAll('h1', attrs={'class': 'pl-header-title'})
+        soup = BeautifulSoup(r.text, "html.parser")
+        name = soup.findAll("h1", attrs={"class": "pl-header-title"})
         self.extract_name(name)
         # soup = soup.findAll('tr', attrs={'class': 'pl-video',
         #                                  'class': 'yt-uix-tile'})
@@ -107,15 +115,14 @@ class YoutubePlaylist(PlaylistBase):
         # this seems rigid against <div> changes
         # so, far this works
         links = soup.find_all(
-            'a',
-            href=re.compile(r".*watch.*")  # this regex can be improved in future
+            "a", href=re.compile(r".*watch.*")  # this regex can be improved in future
         )
         for link in links:
-            href = link['href']
+            href = link["href"]
             title = link.contents[0]
             # If the link is not a video from playlist, there will be no
             # 'index' substring. Hence, we can skip this
-            if 'index' not in href:
+            if "index" not in href:
                 continue
             # Just to make sure the title is not empty. This is done because
             # there is always a first link that contains 'index', yet does not
@@ -144,14 +151,16 @@ class YoutubePlaylist(PlaylistBase):
             self.list_content_tuple.append(YoutubeMetadata(url, title))
 
         if len(self.list_content_tuple) == 0:
-            logger.warning("Are you sure you have videos in your playlist? Try changing\
-                  privacy to public.")
+            logger.warning(
+                "Are you sure you have videos in your playlist? Try changing\
+                  privacy to public."
+            )
 
         self.strip_to_start_end()
 
     def _get_url_slicer(self, url):
         slicers = []
-        strings = ['&index=', '&t=', '&list=']
+        strings = ["&index=", "&t=", "&list="]
         for s in strings:
             try:
                 slicer = url.index(s)
@@ -159,6 +168,92 @@ class YoutubePlaylist(PlaylistBase):
             except ValueError:
                 continue
         return min(slicers)
+
+
+class YoutubePlaylist2(PlaylistBase):
+    """
+        Class to store YouTube playlist data.
+        This uses youtube-dl --flat-playlist command to fetch everything.
+        This is more robust since we don't have to manually parse everything.
+        Plus this solves the issue with ajax/scrolling if playlist has more than
+        100 songs.
+    """
+
+    def __init__(self, URL, pl_start=None, pl_end=None):
+        """Init the URl."""
+        super().__init__(pl_start, pl_end)
+        self.URL = URL
+        self.list_content_tuple = []
+        self.playlist_name = ""
+        self._DELETED = [
+            "deleted video",
+            "मेटाइएको भिडियो",
+            "private video",
+        ]
+
+    def extract_name(self, name):
+        """Extract the name of the playlist."""
+        name = str(name).replace("\n", "")
+        name = "".join(re.findall(r">.*?<", name)).replace(">", "").replace("<", "")
+        name = " ".join(re.findall(r"[^ ]+", name))
+        self.playlist_name = name
+
+    def _is_connection_possible(self):
+        """Make a simple request to check if connection is possible.
+        i:e check if internet is connected.
+        """
+        url = "https://google.com"
+        try:
+            requests.get(url)
+        except requests.exceptions.ConnectionError:
+            return False
+        return True
+
+    def extract_playlistdata(self):
+        url_prepend = "https://www.youtube.com/watch?v="
+        url_base = "https://www.youtube.com"
+        if not self._is_connection_possible():
+            logger.warning("Cannot play playlist. No connection detected!")
+            return "N/A", []
+
+        # first get playlist name
+        logger.info(f"Fetching playlist name for [{self.URL}]")
+        r = requests.get(self.URL)
+        soup = BeautifulSoup(r.text, "html.parser")
+        name = soup.findAll("h1", attrs={"class": "pl-header-title"})
+        self.extract_name(name)
+        logger.info(f"Playlist name = [{self.playlist_name}]")
+
+        logger.info("Fetching songs...")
+        cmd = f"youtube-dl -j --flat-playlist {self.URL}"
+        output, errors = exe(cmd)
+        if not output and errors:
+            logger.error("Unable to extract playlist")
+            return "N/A", []
+        videos = list(map(json.loads, output.split("\n")))
+        logger.info(f"Found {len(videos)} songs")
+        for i, video in enumerate(videos):
+            title = video["title"].strip()
+            url = video["url"]
+            url = url_prepend + url
+            if title.lower()[1:-1] in self._DELETED:
+                logger.debug(title.lower()[1:-1])
+                logger.info(f"Skipping [{url}] Possibly DELETED/BLOCKED/PRIVATE video.")
+                continue
+            self.list_content_tuple.append(YoutubeMetadata(url, title))
+        if len(self.list_content_tuple) == 0:
+            logger.warning(
+                "Are you sure you have videos in your playlist? Try changing\
+                  privacy to public."
+            )
+        self.strip_to_start_end()
+
+        if len(self.list_content_tuple) == 0:
+            logger.warning(
+                "Are you sure you have videos in your playlist? Try changing\
+                  privacy to public."
+            )
+        self.strip_to_start_end()
 
 
 def get_data(URL, pl_start, pl_end):
@@ -170,7 +265,19 @@ def get_data(URL, pl_start, pl_end):
     """
 
     logger.debug("Extracting Playlist Content")
-    youtube_playlist = YoutubePlaylist(URL, pl_start, pl_end)
+    youtube_playlist = YoutubePlaylist2(URL, pl_start, pl_end)
     youtube_playlist.extract_playlistdata()
 
     return youtube_playlist.list_content_tuple, youtube_playlist.playlist_name
+
+
+def main():
+    url = "https://www.youtube.com/playlist?list=PLwg22VSCR0W6cwuCKUJSkX72xEvYXS0Zx"
+    print(url)
+
+    yp = YoutubePlaylist2(url)
+    yp.extract_playlistdata()
+
+
+if __name__ == "__main__":
+    main()
