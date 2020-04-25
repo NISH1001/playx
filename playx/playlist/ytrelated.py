09a910f2cfe6d3fe2652615134a760be879a78de
NISH1001 <nishanpantha@gmail.com>
2020-04-25 15:32:11 +0545

    Add both YTMusic and fallback method while fetching related playlist

--- playx/playlist/ytrelated.py
+++ playx/playlist/ytrelated.py
@@ -1,5 +1,7 @@
 """Scrap Youtube to get the related list from youtube"""
 
+import re
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,6 +9,7 @@ from pathlib import Path
 import json
 
 from youtube_dl import YoutubeDL
+from youtube_dl.utils import DownloadError
 
 from playx.playlist.playlistbase import SongMetadataBase, PlaylistBase
 
@@ -28,6 +31,17 @@ class YoutubeMetadata(SongMetadataBase):
         self.search_query = self.url
 
 
+class CustomYTDLLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        pass
+
+
 class YoutubeRelatedIE(PlaylistBase):
     """
     Get related songs using the YT music endpoint that
@@ -36,13 +50,15 @@ class YoutubeRelatedIE(PlaylistBase):
     We STRICTLY need a video ID or and YoutubeURL, a name
     won't work.
     """
-    def __init__(self, URL):
+
+    def __init__(self, url):
         super().__init__()
-        self.url = self._update_URL(URL)
-        self.playlist_name = ''
+        self.url = url
+        # self.url = self._update_URL(URL)
+        self.playlist_name = ""
         self.youtube_base = "https://www.youtube.com/watch?v={}"
 
-    def _update_URL(self, url):
+    def _get_ytmusic_url(self, url):
         """
         Get the video ID from the URL and add it to
         the YoutubeMusic base URL.
@@ -56,10 +72,11 @@ class YoutubeRelatedIE(PlaylistBase):
         passed URL.
         """
         ydl_opts = {
-            'quiet': True,
-            'nocheckcertificate': True,
-            'dump_single_json': True,
-            'extract_flat': True,
+            "quiet": True,
+            "nocheckcertificate": True,
+            "dump_single_json": True,
+            "extract_flat": True,
+            "logger": CustomYTDLLogger(),
         }
 
         # Extract the info now
@@ -67,9 +84,7 @@ class YoutubeRelatedIE(PlaylistBase):
 
         # Extract the songs into the MetaData class' objects
         for song in songs["entries"]:
-            self.list_content_tuple.append(YoutubeMetadata(
-                song["url"]
-            ))
+            self.list_content_tuple.append(YoutubeMetadata(song["url"]))
         self.playlist_name = songs["title"]
 
     def _create_mix(self):
@@ -83,31 +98,70 @@ class YoutubeRelatedIE(PlaylistBase):
         Since it does all of it using JS, we can't use requests or
         someting similar.
         """
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("–disable-dev-shm-usage")
-        chrome_options.add_argument("--remote-debugging-port=9222")
-        driver = webdriver.Chrome(options=chrome_options)
-
+        logger.info("Using YTMusic Method")
+        driver = self._get_driver()
         driver.get(self.url)
-        WebDriverWait(driver, 10).until(
-            lambda driver: driver.current_url != self.url
-        )
+        WebDriverWait(driver, 10).until(lambda driver: driver.current_url != self.url)
 
         # The URL should now be updated
         updated_url = driver.current_url
         playlist_id = updated_url.split("=")[-1]
-        playlist_url = "https://www.youtube.com/playlist?list={}".format(
-            playlist_id
-        )
+        playlist_url = "https://www.youtube.com/playlist?list={}".format(playlist_id)
         self._get_playlist_data(playlist_url)
+
+    def _not_name(self, name):
+        """
+        Check the passed name to see if its actually a name of song.
+
+        While extracting sometimes playlists are suggested in which
+        the extraction algo extracts the time of the playlist instead
+        of the name, so we need to remove it from the list of songs.
+        """
+        match = re.match(r"[0-9][0-9]?:[0-9][0-9]", name)
+        if match is None:
+            return False
+        else:
+            return True
+
+    def _get_driver(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("–disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--remote-debugging-port=9222")
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option("useAutomationExtension", False)
+        return webdriver.Chrome(options=chrome_options)
+
+    def _extract_fallback(self):
+        logger.info("Using fallback method")
+        driver = self._get_driver()
+        driver.implicitly_wait(10)
+        driver.get(self.url)
+        songs = driver.find_elements_by_tag_name("ytd-compact-video-renderer")
+        logger.debug(str(len(songs)))
+
+        for i in songs:
+            contents = i.text.split("\n")
+            song_name = contents[0]
+            logger.debug(song_name)
+            if not self._not_name(song_name):
+                self.list_content_tuple.append(YoutubeMetadata(song_name))
+        driver.quit()
 
     def extract_songs(self):
         """
         Generic method to start all the fetching related work.
         """
-        self._create_mix()
+        original_url = self.url
+        try:
+            self.url = self._get_ytmusic_url(original_url)
+            self._create_mix()
+        except DownloadError:
+            self.url = original_url
+            self._extract_fallback()
 
 
 def get_data(url):
@@ -120,9 +174,9 @@ def get_data(url):
 
     logger.debug("Checking related playlist cache")
     # Check if FILE_NAME is present in CACHE_PATH
-    for file in CACHE_PATH.iterdir():
-        logger.debug("{}".format(file))
-        if file == FILE_PATH:
+    for fname in CACHE_PATH.iterdir():
+        logger.debug("{}".format(fname))
+        if fname == FILE_PATH:
             # Extract the data from FILE_PATH
             with open(FILE_PATH) as RSTREAM:
                 data = json.load(RSTREAM)[1]["data"]
@@ -156,7 +210,7 @@ def save_data(url, data):
     # Make the data a bit proper
     data_ = []
     for entity in data:
-        data_.append(entity.title)
+        data_.append(entity.title if entity.title else entity.search_query)
 
     with open(FILE_PATH, "w") as WSTREAM:
         DATA = [{"URL": url}, {"data": data_}]
@@ -166,6 +220,7 @@ def save_data(url, data):
 if __name__ == "__main__":
     print("Debugging ytrelated...")
     url = "https://www.youtube.com/watch?v=fdubeMFwuGs"
+    # url = "https://www.youtube.com/watch?v=bSxJp3dE0HY"
     print(f"url = {url}")
     d = get_data(url)
     for i in d:
--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup
 
 setup(
-    name='playx',
-    packages=['playx', 'playx.playlist'],
-    author='NISH1001',
-    author_email='nishanpantha@gmail.com',
-    description='Search and play any song from terminal.',
-    long_description='Search and play any song from terminal seamlessly.',
-    url='http://github.com/NISH1001/playx',
-    entry_points={
-        'console_scripts': [
-            'playx = playx.main:main'
-        ]
-    },
-    version='1.5.0',
-    license='MIT',
-    install_requires=['youtube_dl', 'requests', 'beautifulsoup4', 'selenium', 'lxml', 'numpy'],
+    name="playx",
+    packages=["playx", "playx.playlist"],
+    author="NISH1001",
+    author_email="nishanpantha@gmail.com",
+    description="Search and play any song from terminal.",
+    long_description="Search and play any song from terminal seamlessly.",
+    url="http://github.com/NISH1001/playx",
+    entry_points={"console_scripts": ["playx = playx.main:main"]},
+    version="1.5.1",
+    license="MIT",
+    install_requires=[
+        "youtube_dl",
+        "requests",
+        "beautifulsoup4",
+        "selenium",
+        "lxml",
+        "numpy",
+    ],
 )
